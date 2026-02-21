bec7ae86070b20a3c2d320f815cca870770c2444
Rohit Nawale <156205538+coding-with-rohit-914@users.noreply.github.com>
2026-02-21 13:15:57 +0530

    Update wsgi.py

--- slms/wsgi.py
+++ slms/wsgi.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slms.settings')
 # application = get_wsgi_application()
 
+# slms/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slms.settings')
 
-app = get_wsgi_application()  # Vercel looks for 'app'
+# IMPORTANT: Vercel looks for 'app' variable
+app = get_wsgi_application()
